80f09f4c58b6aeec0d6f7e42394539c51574717c
kaioFerreira <kaiofhs@hotmail.com>
2022-09-12 14:38:15 -0300

    Version 4.0

--- i18n.config.js
+++ i18n.config.js
@@ -12,8 +12,8 @@ const i18n = require("i18n"),
   path = require("path");
 
 i18n.configure({
-  locales: ["en_US"],
-  defaultLocale: "en_US",
+  locales: ["pt_BR"],
+  defaultLocale: "pt_BR",
   directory: path.join(__dirname, "locales"),
   objectNotation: true,
   api: {
